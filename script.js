39982224faed8d4de3b4c6b8e718285e6043583d
XIIIX89 <114373945+XIIIX89@users.noreply.github.com>
2024-01-08 14:38:53 -0800

    Fully functional with variable grid and box size

--- script.js
+++ script.js
@@ -2,13 +2,15 @@ const body = document.body;
 const mainBox = body.querySelector('#mainBox');
 const inputsBox = body.querySelector('#inputsBox');
 let rowNumber = 0;
-let resSize = 20;
+let resSize = body.querySelector('#resSize');
+
 
 inputsBox.addEventListener('click', (e) => {
     let target = e.target.id;
     switch(target) {
         case 'newGridBtn': 
         mainBox.innerHTML = ''; //removes everything from mainBox
+        //resSize = 
         let dimensions = parseInt(prompt("Enter dimensions (one number. <= 100):"));
         if(dimensions > 100) {
             alert("Size out of boundaries!")
@@ -16,7 +18,7 @@ inputsBox.addEventListener('click', (e) => {
         }
         dimensions--; // accounts for 0 index of dimensions so its accurate to what the user inputs
         mainBox.style = 'border: 4px solid black;'
-        createRows(dimensions); // creates the number of rows specified by user, then calls the function to fill them with boxes of the same dimensions
+        createRows(dimensions, resSize.value); // creates the number of rows specified by user, then calls the function to fill them with boxes of the same dimensions
         break;
     }
 });
@@ -27,18 +29,18 @@ mainBox.addEventListener('mouseover', (e) => {
 });
 
 // creates the flexbox row divs that contain the smaller boxes
-function createRows(dimensions) {
+function createRows(dimensions, resSize) {
     for (let i = 0; i <= dimensions; i++) {
         const rowDiv = document.createElement('div');
         mainBox.append(rowDiv);
         rowDiv.classList.toggle('rowDiv');
-        fillRow(dimensions, rowDiv, rowNumber);
+        fillRow(dimensions, rowDiv, rowNumber, resSize);
         rowNumber += 1;
     }
 }
 
 // fills rows with boxes 20px square, called in createRows function each loop. These boxes are identified as R#C# for row #, column #
-function fillRow(dimensions, rowDiv, rowNumber) {
+function fillRow(dimensions, rowDiv, rowNumber, resSize) {
     for (let columnNumber = 0; columnNumber <= dimensions; columnNumber++) {
         const boxDiv = document.createElement('div');
         rowDiv.append(boxDiv);
