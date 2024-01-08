ccd078fdcabd27580025be6e0e56b249ccfbccee
XIIIX89 <114373945+XIIIX89@users.noreply.github.com>
2024-01-08 14:59:31 -0800

    stopped rows from continuing to count with new iterations of grid

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let resSize = body.querySelector('#resSize');
 
 
 inputsBox.addEventListener('click', (e) => {
+    rowNumber = 0;
     let target = e.target.id;
     switch(target) {
         case 'newGridBtn': 
