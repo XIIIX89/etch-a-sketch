e15f3c495dc0fb18b818f04568a88a97269bf0f4
XIIIX89 <114373945+XIIIX89@users.noreply.github.com>
2024-01-08 11:42:47 -0800

    New Grid button now removes old grid before creating a new one.

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 const body = document.body;
 const mainBox = body.querySelector('#mainBox');
+const inputsBox = body.querySelector('#inputsBox');
 let rowNumber = 0;
 let resSize = 20;
 
-
-let dimensions = parseInt(prompt("Enter dimensions (one number. <= 100):"));
-if(dimensions > 100) {
-
-    location.reload();
-}
-dimensions--; // accounts for 0 index of dimensions so its accurate to what the user inputs
-createRows(dimensions); // creates the number of rows specified by user, then calls the function to fill them with boxes of the same dimensions
-
+inputsBox.addEventListener('click', (e) => {
+    let target = e.target.id;
+    switch(target) {
+        case 'newGridBtn': 
+        mainBox.innerHTML = ''; //removes everything from mainBox
+        let dimensions = parseInt(prompt("Enter dimensions (one number. <= 100):"));
+        if(dimensions > 100) {
+            alert("Size out of boundaries!")
+            newGridBtn.click();
+        }
+        dimensions--; // accounts for 0 index of dimensions so its accurate to what the user inputs
+        mainBox.style = 'border: 4px solid black;'
+        createRows(dimensions); // creates the number of rows specified by user, then calls the function to fill them with boxes of the same dimensions
+        break;
+    }
+});
 
 mainBox.addEventListener('mouseover', (e) => {
     let target = e.target;
@@ -35,6 +43,7 @@ function fillRow(dimensions, rowDiv, rowNumber) {
         const boxDiv = document.createElement('div');
         rowDiv.append(boxDiv);
         boxDiv.classList.toggle('boxDiv'); // this defaults the resSize to 20px
+        boxDiv.style = `min-width: ${resSize}px; min-height: ${resSize}px;`; // sets the resolution size for the divBoxes
         boxDiv.id = 'R' + rowNumber + 'C' + columnNumber;
     }
 }
